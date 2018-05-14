9f4dcbfaf3e53c8c3b5d848ca59d2dc9b36f50eb
Nikolai Pohorelsky <nikolai.pohorelsky@showtime.net>
2018-05-14 16:32:46 -0400

    1. Added filename to the progress meter

--- plugins/progress.js
+++ plugins/progress.js
@@ -8,12 +8,15 @@ class Progress {
   }
 
   fileStart(filename, numLines) {
-    const tpl = this._options.template || ':bar :percent :eta';
+    this.filename = filename;
+    const tpl = this._options.template || ':filename :bar :percent :eta';
     this._bar = new ProgressBar(tpl, { total: numLines });
   }
 
   line(index, numLines, line) {
-    this._bar.tick();
+    this._bar.tick({
+      filename: this.filename
+    });
   }
 }
 
